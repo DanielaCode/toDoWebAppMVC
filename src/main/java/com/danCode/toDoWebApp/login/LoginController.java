f0420cb2b2929cae4c099117a4fa72ba1ee85a03
Daniela <danielagp96@gmail.com>
2023-06-03 10:39:32 -0600

    caching query params from the browser and binding it with java variable

--- src/main/java/com/danCode/toDoWebApp/login/LoginController.java
+++ src/main/java/com/danCode/toDoWebApp/login/LoginController.java
@@ -2,11 +2,14 @@ package com.danCode.toDoWebApp.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    //http://localhost:8081/login-jsp?name=dani
     @RequestMapping("login-jsp")
-    public String login(){
+    public String login(@RequestParam String name){ //request param is binding what the client sends with this String name
+        System.out.println(name);//not recommended for production
         return "login";
     }
 }
