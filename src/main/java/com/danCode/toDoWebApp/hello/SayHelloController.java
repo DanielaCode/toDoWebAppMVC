e3cd4097d031591f397aa142ce45f0270e0b66e0
Daniela <danielagp96@gmail.com>
2023-06-03 09:59:25 -0600

    Adding an endpoint that return a jsp file and soing the corresponding settings that springboot recommends

--- src/main/java/com/danCode/toDoWebApp/hello/SayHelloController.java
+++ src/main/java/com/danCode/toDoWebApp/hello/SayHelloController.java
@@ -13,10 +13,12 @@ public class SayHelloController {
         return "Hello";
     }
 
+    //this is just and example because this is not scalable, so we are going to use Views tho do this one
     @RequestMapping("say-hello-html")
     @ResponseBody
     public  String sayHelloHtml(){
         //return strings is not considered efficient so go for stringBuilder or string buffer
+        //JSP java software pages view technology popular
         StringBuffer sb = new StringBuffer();
         sb.append("<html>");
         sb.append("<head>");
@@ -27,5 +29,14 @@ public class SayHelloController {
         sb.append("</body>");
         sb.append("</html>");
         return sb.toString();
+    }
+
+    //url:"say-hello-jsp  => sayHello.jsp
+
+    //@ResponseBody because this return directly what is the function returned
+    //and I don't want it, I want to return my jsp, so I set in application.properties suffix and prefix
+    @RequestMapping("say-hello-jsp")
+    public  String sayHelloJsp(){
+        return "sayHello";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,9 @@
+
+<html>
+<head>
+    <title>first view with JSP</title>
+</head>
+<body>
+    first view with JSP
+</body>
+</html>
