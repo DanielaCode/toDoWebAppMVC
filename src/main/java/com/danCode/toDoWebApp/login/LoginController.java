d063035838c5b4c91d513297623e9a5875249249
Daniela <danielagp96@gmail.com>
2023-06-03 16:19:26 -0600

    adding form to the login page

--- src/main/java/com/danCode/toDoWebApp/login/LoginController.java
+++ src/main/java/com/danCode/toDoWebApp/login/LoginController.java
@@ -13,15 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
-
-    //http://localhost:8081/login-jsp?name=dani
-    @RequestMapping("login-jsp")
-    public String login(@RequestParam String name, ModelMap model){ //request param is binding what the client sends with this String name
-        //instead of use sout
-        logger.debug("request param is"+name);
-        //how can I use this variable to put it on my html? => with Model
-        //whatever I want to use on jsp I put it into model
-        model.put("name",name);
+    @RequestMapping("login")
+    public String login(){
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,6 +4,12 @@
     <title>login view with JSP</title>
 </head>
 <body>
-    Welcome dear ${name}
+    Welcome dear
+    <form method="post">
+        Name: <input type="text" name="name">
+        Password: <input type="password" name="password">
+
+        <input type="submit">
+    </form>
 </body>
 </html>
