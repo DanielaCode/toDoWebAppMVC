7e768d0828ad7541f4913b19be57f2705b1be01b
Daniela <danielagp96@gmail.com>
2023-06-04 18:39:32 -0600

    scope tag on pom is to include or not on the jar file, a lot had ben created for spring boot security  but this is avoiding to be able to connect to h2.console so we I to override SecurityFilterChain in security configurations

--- src/main/java/com/danCode/toDoWebApp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/danCode/toDoWebApp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.danCode.toDoWebApp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -42,5 +45,30 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /*spring boot security have the following setting by default:
+        1. All urls are protected
+        2. a login form is showing for unauthorized requests
+        3. CSRF disable
+        4. does not allow html Frames
+
+        because we are overriding the security filter chain we need to define it again:
+    */
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //1.
+        http.authorizeHttpRequests(
+            auth -> auth.anyRequest().authenticated()
+        );
+        //2.
+        http.formLogin(Customizer.withDefaults());
+        //3.
+        http.csrf(csrf -> csrf.disable());
+        //4. https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/config/annotation/web/builders/HttpSecurity.html#headers(org.springframework.security.config.Customizer)
+        http.headers(headers-> headers.frameOptions((frameOptions) -> frameOptions.disable()));
+
+        return http.build();
     }
 }
